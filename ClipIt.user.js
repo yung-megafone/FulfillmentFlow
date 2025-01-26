193b5284197d488c722be10b8857b0db21a2e2b3
Brando <104199664+yung-megafone@users.noreply.github.com>
2025-01-26 05:03:31 -0600

    ClipIt v0.1

--- ClipIt.user.js
+++ ClipIt.user.js
@@ -7,8 +7,8 @@
 // @include      http*://t.corp.amazon.com/*
 // @run-at       document-end
 // @grant        GM_setClipboard
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://raw.githubusercontent.com/yung-megafone/FulfillmentFlow/refs/heads/main/ClipIt.user.js
+// @downloadURL  https://raw.githubusercontent.com/yung-megafone/FulfillmentFlow/refs/heads/main/ClipIt.user.js
 // ==/UserScript==
 
 (function() {
